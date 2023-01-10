659403aa8fef4d8b8a87664e0beb69c3b282c5c7
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2023-01-10 10:25:30 -0500

    created constructor and initialized variables

--- src/cpt/Country.java
+++ src/cpt/Country.java
@@ -2,6 +2,12 @@ package cpt;
 
 public class Country {
     
-    
+    private String name;
+    private int year;
+    private int alcConsumption;
+
+    public Country(String countryName, int countryYear, int countryAlcConsum){
+        
+    }
 
 }
