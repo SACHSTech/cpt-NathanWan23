7590b4fd6680f8f846ba8fea226297873bb10512
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2023-01-21 02:46:44 -0500

    Delted main and added horizontal bar chart cpt

--- src/cpt/cpt.java
+++ src/cpt/cpt.java
@@ -16,25 +16,29 @@ import javafx.stage.Stage;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 
+import java.util.Arrays;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
 public class cpt extends Application{
     
-/**
- * A chart that displays rectangular bars with heights indicating data values
- * for categories. Used for displaying information when at least one axis has
- * discontinuous or discrete data.
- */
-
-    
-    
-  private BarChart chart;
+  /*private BarChart chart;
   private CategoryAxis xAxis;
-  private NumberAxis yAxis;
+  private NumberAxis yAxis;*/
 
     /*private LineChart chart;
     private NumberAxis xAxis;
     private NumberAxis yAxis;*/
 
-    public Parent createContent() {
+    /*public Parent createContent() {
         String[] years = {"2007", "2008", "2009"};
         int[] data = {567, 1292, 1292};
 
@@ -58,32 +62,58 @@ public class cpt extends Application{
             );
         chart = new BarChart(xAxis, yAxis, barChartData, 25.0d);
 
-        /*xAxis = new NumberAxis("Values for X-Axis", 0, 3, 1);
-        yAxis = new NumberAxis("Values for Y-Axis", 0, 3, 1);
-        ObservableList<XYChart.Series<Double,Double>> lineChartData =
-            FXCollections.observableArrayList(
-                new LineChart.Series<>("Series 1",
-                                       FXCollections.observableArrayList(
-                    new XYChart.Data<>(0.0, 1.0),
-                    new XYChart.Data<>(1.2, 1.4),
-                    new XYChart.Data<>(2.2, 1.9),
-                    new XYChart.Data<>(2.7, 2.3),
-                    new XYChart.Data<>(2.9, 0.5))),
-                new LineChart.Series<>("Series 2",
-                                       FXCollections.observableArrayList(
-                    new XYChart.Data<>(0.0, 1.6),
-                    new XYChart.Data<>(0.8, 0.4),
-                    new XYChart.Data<>(1.4, 2.9),
-                    new XYChart.Data<>(2.1, 1.3),
-                    new XYChart.Data<>(2.6, 0.9)))
-            );
-        chart = new LineChart(xAxis, yAxis, lineChartData);
-        return chart;*/
+        
     //}
 
         return chart;
         
-  }
+  } */
+
+  //public class HorizontalBarChartApp extends Application {
+ 
+    private BarChart<Number, String> chart;
+    private NumberAxis xAxis;
+    private CategoryAxis yAxis;
+ 
+    public Parent createContent() {
+        final String[] years = {"2007", "2008", "2009"};
+        final ObservableList<String> categories =
+            FXCollections.<String>observableArrayList(Arrays.asList(years));
+        xAxis = new NumberAxis();
+        yAxis = new CategoryAxis();
+        chart = new BarChart<>(xAxis, yAxis);
+        chart.setTitle("Horizontal Bar Chart Example");
+        yAxis.setLabel("Year");
+        yAxis.setCategories(categories);
+        xAxis.setLabel("Price");
+ 
+        // add starting data
+        XYChart.Series<Number, String> series1 = new XYChart.Series<>();
+        series1.setName("Data Series 1");
+        series1.getData().addAll(
+                new XYChart.Data<Number, String>(567, years[0]),
+                new XYChart.Data<Number, String>(1292, years[1]),
+                new XYChart.Data<Number, String>(2180, years[2]));
+ 
+        XYChart.Series<Number, String> series2 = new XYChart.Series<>();
+        series2.setName("Data Series 2");
+        series2.getData().addAll(
+                new XYChart.Data<Number, String>(956, years[0]),
+                new XYChart.Data<Number, String>(1665, years[1]),
+                new XYChart.Data<Number, String>(2450, years[2]));
+ 
+        XYChart.Series<Number, String> series3 = new XYChart.Series<>();
+        series3.setName("Data Series 3");
+        series3.getData().addAll(
+                new XYChart.Data<Number, String>(800, years[0]),
+                new XYChart.Data<Number, String>(1000, years[1]),
+                new XYChart.Data<Number, String>(2800, years[2]));
+ 
+        chart.getData().add(series1);
+        chart.getData().add(series2);
+        chart.getData().add(series3);
+        return chart;
+    }
  
     @Override public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene(new Scene(createContent()));
