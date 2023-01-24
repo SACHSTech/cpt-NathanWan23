755a7cd70562176d58b2ae835692a8ce9e465884
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2023-01-24 09:16:52 -0500

    cleaned up code

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -26,7 +26,6 @@ import javafx.scene.control.Button;
 //Imports for VBox and stacked layout
 import javafx.scene.layout.VBox;
 import javafx.geometry.Insets;
-import javafx.scene.layout.StackPane;
 
 public class Main extends Application{
     
@@ -52,7 +51,6 @@ public class Main extends Application{
     private XYChart.Series<Number, String> series4 = new XYChart.Series<>();
     
     public Parent horizontalBarChartApp() throws IOException {
-    //public Parent createContent() throws IOException{
         DataCollection DataCollection = new DataCollection();
         ArrayList<String> listCountryName = new ArrayList<String>();
         
@@ -63,68 +61,38 @@ public class Main extends Application{
 
         listCountryName = DataCollection.countryName();
 
-        //final ObservableList<String> categories =
             FXCollections.<String>observableArrayList(listCountryName);
         xAxis = new NumberAxis();
         yAxis = new CategoryAxis();
         chart = new BarChart<>(xAxis, yAxis);
         chart.setTitle("Total Alcohol Consumption per Capita (Liters of Pure Alcohol, 15+ age)");
         yAxis.setLabel("Country");
-        //yAxis.setCategories(categories);
         xAxis.setLabel("Total Pure Alcohol Consumption per Person over 15 years of age");
  
-        //XYChart.Series<Number, String> series1 = new XYChart.Series<>();
         series1.setName("2000");
         for(int i = 0; i<year1.size();i++){
             series1.getData().add(
             new XYChart.Data<Number, String>(year1.get(i).getAlcConsumption(), year1.get(i).getName()));                
         }
 
-        //XYChart.Series<Number, String> series2 = new XYChart.Series<>();
         series2.setName("2010");
         for(int i = 0; i<year2.size();i++){
             series2.getData().add(
             new XYChart.Data<Number, String>(year2.get(i).getAlcConsumption(), year2.get(i).getName()));     
         }
 
-        //XYChart.Series<Number, String> series3 = new XYChart.Series<>();
         series3.setName("2015");
         for(int i = 0; i<year3.size();i++){
             series3.getData().add(
             new XYChart.Data<Number, String>(year3.get(i).getAlcConsumption(), year3.get(i).getName()));          
         }
     
-        //XYChart.Series<Number, String> series4 = new XYChart.Series<>();
             series4.setName("2018");
             for(int i = 0; i<year4.size();i++){
                 series4.getData().add(
                 new XYChart.Data<Number, String>(year4.get(i).getAlcConsumption(), year4.get(i).getName()));
             }
         
-        /*series1.setName("Data Series 1");
-        series1.getData().addAll(
-                new XYChart.Data<Number, String>(567, years[0]),
-                new XYChart.Data<Number, String>(1292, years[1]),
-                new XYChart.Data<Number, String>(2180, years[2]));
- 
-        XYChart.Series<Number, String> series2 = new XYChart.Series<>();
-        series2.setName("Data Series 2");
-        series2.getData().addAll(
-                new XYChart.Data<Number, String>(956, years[0]),
-                new XYChart.Data<Number, String>(1665, years[1]),
-                new XYChart.Data<Number, String>(2450, years[2]));
- 
-        XYChart.Series<Number, String> series3 = new XYChart.Series<>();
-        series3.setName("Data Series 3");
-        series3.getData().addAll(
-                new XYChart.Data<Number, String>(800, years[0]),
-                new XYChart.Data<Number, String>(1000, years[1]),
-                new XYChart.Data<Number, String>(2800, years[2])); */
- 
-        //chart.getData().add(series1);
-        //chart.getData().add(series2);
-        //chart.getData().add(series3);
-        //chart.getData().add(series4);
         chart.setPrefHeight(600);
         chart.setPrefWidth(500);
         return chart;
@@ -137,31 +105,95 @@ public class Main extends Application{
         ArrayList<Double> countryData = new ArrayList<Double>();
 
 
-        xAxis2 = new NumberAxis("Total Alcohol Consumption per Capita (Liters of Pure Alcohol, 15+ age)", 0, 20, 1);
-        yAxis2 = new NumberAxis("Values for Y-Axis", 2000, 2018, 1);
+        xAxis2 = new NumberAxis("Total Alcohol Consumption per Capita (Liters of Pure Alcohol, 15+ age)", 0, 20, 0.5);
+        yAxis2 = new NumberAxis("Years", 2000, 2020, 1);
         //ObservableList<XYChart.Series<Double,Double>> lineChartData =
-            FXCollections.<Double>observableArrayList(countryData);
-            for(int i = 0; i <listCountryName.size(); i++){
+        
+        ObservableList<XYChart.Series<Double,Double>> lineChartData = FXCollections.observableArrayList();
+        
+        for(int i = 0; i< listCountryName.size();i++){
+            ObservableList<XYChart.Data> seriesData = FXCollections.observableArrayList();
 
-                countryData = dataCollection.countryData(listCountryName.get(i));
-                LineChart.Series<Double, Double> series = new LineChart.Series<Double, Double>();
+            countryData = dataCollection.countryData(listCountryName.get(i));
+
+            for(int j = 0; j < 8; j+=2){
+
+                seriesData.add( new XYChart.Data<>(countryData.get(j+1), countryData.get(j)));
+                
+                
+            }
+            XYChart.Series series = new XYChart.Series();
+            series.setData(seriesData);
+            series.setName(listCountryName.get(i));
+            lineChartData.add(series);
+        }
+
+        ///////////
+        //ObservableList<XYChart.Data> seriesData = FXCollections.observableArrayList();
+        /*for(int i = 0; i< listCountryName.size();i++){
+            ObservableList<XYChart.Data> seriesData = FXCollections.observableArrayList();
+
+            countryData = dataCollection.countryData(listCountryName.get(i));
+
+            
+        //ObservableList<XYChart.Series<Double,Double>> series = FXCollections.observableArrayList();
+        //ObservableList<XYChart.Series<Double,Double>> lineChartData = FXCollections.observableArrayList();
+            //FXCollections.observableArrayList(
+            //FXCollections.<Double>observableArrayList(countryData);
+            
+            ////////
+            for(int j = 0; j < 8; j+=2){
+
+                // this would be generated by a loop
+                seriesData.add( new XYChart.Data<>(countryData.get(j+1), countryData.get(j)));
+                //System.out.println(countryData.get(j) + ", " +countryData.get(j+1));
+                //System.out.println(countryData.get(j) + " " + countryData.get(j+1));
+                /*LineChart.Series<Double, Double> series = new LineChart.Series<Double, Double>();
                 series.getData().addAll(
                     new XYChart.Data<Double, Double>(countryData.get(0), countryData.get(1)),
                     new XYChart.Data<Double,Double>(countryData.get(2), countryData.get(3)),
                     new XYChart.Data<Double,Double>(countryData.get(4), countryData.get(5)),
                     new XYChart.Data<Double,Double>(countryData.get(6), countryData.get(7)));
 
-                chart2.getData().add(series);
+                chart2.getData().add(series);*/
+
                 /*new LineChart.Series<Double,Double>(listCountryName.get(i),
-                    FXCollections.observableArrayList(
+                                                    FXCollections.observableArrayList(
                         new XYChart.Data<Double,Double>(countryData.get(0), countryData.get(1)),
                         new XYChart.Data<Double,Double>(countryData.get(2), countryData.get(3)),
                         new XYChart.Data<Double,Double>(countryData.get(4), countryData.get(5)),
                         new XYChart.Data<Double,Double>(countryData.get(6), countryData.get(7))));*/
+                        //LineChart.Series series = new LineChart.Series<Double,Double>();
+                                                    /*FXCollections.observableArrayList(
+                        new XYChart.Data<Double,Double>(countryData.get(0), countryData.get(1)),
+                        new XYChart.Data<Double,Double>(countryData.get(2), countryData.get(3)),
+                        new XYChart.Data<Double,Double>(countryData.get(4), countryData.get(5)),
+                        new XYChart.Data<Double,Double>(countryData.get(6), countryData.get(7)));
+                        series.setName(listCountryName.get(i));*/
                 
-            }
+            //}
+            //XYChart.Series series = new XYChart.Series();
+            //series.setData(seriesData);
+            //series.setName(listCountryName.get(i));
+            //lineChartData.add(series);
+        //}
+           // );
+
+           /*ObservableList<XYChart.Data> series1Dat = FXCollections.observableArrayList();
+
+        // this would be generated by a loop
+        series1Dat.add( new XYChart.Data<>(0.0, 2000.0));
+        series1Dat.add( new XYChart.Data<>(1.2, 2140));
+        series1Dat.add( new XYChart.Data<>(2.2, 190));
+        series1Dat.add( new XYChart.Data<>(2.7, 2.3));
+        series1Dat.add( new XYChart.Data<>(2.9, 0.5));
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setData(series1Dat);
+
+        lineChartData.add(series1);*/
             
-        chart2 = new LineChart(xAxis2, yAxis2);
+        chart2 = new LineChart(xAxis2, yAxis2,lineChartData);
         return chart2;
     }
 
@@ -201,11 +233,12 @@ public class Main extends Application{
         //Layout 1
         VBox layout1 = new VBox(10);
         layout1.setPadding(new Insets(20,20,20,20));
-        layout1.getChildren().addAll(box1, box2, box3, box4, checkButton, horizontalBarChartApp(), buttonScene2);
+        layout1.getChildren().addAll(box1, box2, box3, box4, checkButton, lineChartApp(), buttonScene2);
         scene1 = new Scene(layout1, 600, 600);
 
         //Layout 2
-        StackPane layout2 = new StackPane();
+        VBox layout2 = new VBox(10);
+        //StackPane layout2 = new StackPane();
         layout2.setPadding(new Insets(20,20,20,20));
         layout2.getChildren().addAll(lineChartApp(),buttonScene1);
         scene2 = new Scene(layout2, 600, 600);         
