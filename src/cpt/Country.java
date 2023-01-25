dd9272a3c2e34315fefbc4c15b977f21aa6c7aa5
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2023-01-25 08:59:03 -0500

    comments and javadoc for country

--- src/cpt/Country.java
+++ src/cpt/Country.java
@@ -1,25 +1,49 @@
 package cpt;
 
+/**
+ * Creating coutry objects (data objects)
+ * @author: N. Wan
+ * 
+ */
 public class Country {
     
+    //Initialize variables
     private String name;
     private int year;
     private double alcConsumption;
 
+    /**
+    * data point constructor
+    * @param countryName country's name
+    * @param countryYear data point's year
+    * @param countryAlcConsum data for that country from that year
+    */
     public Country(String countryName, int countryYear, double countryAlcConsum){
         this.name = countryName;
         this.year = countryYear;
         this.alcConsumption = countryAlcConsum;
     }
 
+    /**
+    * getter name method
+    @return name returns the name of the country
+    */
     public String getName(){
         return name;
     }
 
+    /**
+    * getter year method
+    * @return year that this data point is 
+    */
     public int getYear(){
         return year;
     }
 
+    /**
+    * getter alcohol consumption method
+    * @return alcConsumption returns the alcohol consumption for that country in a specific data point year
+    */
     public double getAlcConsumption(){
         return alcConsumption;
     }
